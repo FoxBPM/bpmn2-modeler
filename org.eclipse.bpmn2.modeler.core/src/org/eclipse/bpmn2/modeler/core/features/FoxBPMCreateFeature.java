72cdb03522eb6335280c7355ddf3a1aa4f5bcb2a
demonrain <demonrain@vip.qq.com>
2014-06-30 10:28:21 +0800

    修复lane节点拖上去不显示问题

--- org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/core/features/FoxBPMCreateFeature.java
+++ org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/core/features/FoxBPMCreateFeature.java
@@ -1,9 +1,15 @@
 package org.eclipse.bpmn2.modeler.core.features;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.bpmn2.BaseElement;
+import org.eclipse.bpmn2.Bpmn2Factory;
 import org.eclipse.bpmn2.Definitions;
 import org.eclipse.bpmn2.DocumentRoot;
 import org.eclipse.bpmn2.FlowElement;
+import org.eclipse.bpmn2.Lane;
+import org.eclipse.bpmn2.LaneSet;
 import org.eclipse.bpmn2.Process;
 import org.eclipse.bpmn2.di.BPMNEdge;
 import org.eclipse.bpmn2.di.BPMNShape;
@@ -81,6 +87,67 @@ public class FoxBPMCreateFeature extends CompoundCreateFeature {
 				AddContext ac = new AddContext(new AreaContext(), newFlowElement);
 				ac.setTargetContainer(diagram);
 				ac.setNewObject(newFlowElement);
+				if (null != newBpmnShape) {
+					float x1 = newBpmnShape.getBounds().getX();
+					float y1 = newBpmnShape.getBounds().getY();
+					int x = (int) x1;
+					int y = (int) y1;
+					ac.setLocation(x, y);
+				}
+
+				IAddFeature af = getFeatureProvider().getAddFeature(ac);
+				ac.setX(((ICreateContext) context).getX());
+				ac.setY(((ICreateContext) context).getY());
+				af.add(ac);
+			} else if (bpmnEdge != null) {
+//				DIUtils.addShape(bpmnEdge, definitions.getDiagrams().get(0));
+			}
+		}
+		
+		//Lane
+		List<Lane> lanes = new ArrayList<Lane>();
+		for (LaneSet laneSet : templateProcess.getLaneSets()) {
+			lanes.addAll(laneSet.getLanes());
+		}
+		
+		for (Lane lane : lanes) {
+			Lane lane2 = EcoreUtil.copy(lane);
+			lane2.setId(null);
+			ModelUtil.setID(lane2, process.eResource());
+
+			TreeIterator<EObject> contents = lane2.eAllContents();
+			for (; contents.hasNext();) {
+				EObject t = contents.next();
+				if (t instanceof BaseElement) {
+					((BaseElement) t).setId(null);
+					ModelUtil.setID(t, process.eResource());
+				}
+			}
+
+			//现在process类型的流程默认只有一个lanSet
+			if(process.getLaneSets().size()<1) {
+				LaneSet laneSet = Bpmn2Factory.eINSTANCE.createLaneSet();
+				ModelUtil.setID(laneSet, process.eResource());
+				laneSet.setName("Lane Set 1");
+				process.getLaneSets().add(laneSet);
+			}
+			process.getLaneSets().get(0).getLanes().add(lane2);
+
+			BPMNShape newBpmnShape = null;
+
+			BPMNShape bpmnShape = DIUtils.findBPMNShape(lane);
+			BPMNEdge bpmnEdge = DIUtils.findBPMNEdge(lane);
+			if (bpmnShape != null) {
+				newBpmnShape = EcoreUtil.copy(bpmnShape);
+
+				newBpmnShape.setId(null);
+				ModelUtil.setID(newBpmnShape, process.eResource());
+				newBpmnShape.setBpmnElement(lane2);
+				plane.getPlaneElement().add(newBpmnShape);
+
+				AddContext ac = new AddContext(new AreaContext(), lane2);
+				ac.setTargetContainer(diagram);
+				ac.setNewObject(lane2);
 				if (null != newBpmnShape) {
 					float x1 = newBpmnShape.getBounds().getX();
 					float y1 = newBpmnShape.getBounds().getY();
